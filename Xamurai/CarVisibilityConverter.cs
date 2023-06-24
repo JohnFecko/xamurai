6de07e7cf201356f8f53ad27b05f023a6e1308de
John Fecko <john@fecko.me>
2023-06-23 20:25:08 -0400

    Add changed font and gradient background for Mercedes

---
+++ Xamurai/CarVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Xamurai
+{
+    
+    public class CarDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate CarDataTemplate { get; set; }
+        public DataTemplate MercedesDataTemplate { get; set; }
+
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            Car car = (Car)item;
+            if(car.Make == CarMake.Mercedes)
+            {
+                return MercedesDataTemplate;
+            }
+            return CarDataTemplate;
+        }
+    }
+}
+
---
+++ Xamurai/MercedesView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Xamurai
+{
+    public class MercedesView : CarView
+    {
+        public MercedesView()
+        {
+
+            Style labelStyle = new Style(typeof(Label))
+            {
+                Setters = {
+                    new Setter
+                    {
+                        Property = Label.FontFamilyProperty,
+                        Value =  "OpenSans"
+                    }
+                }
+            };
+            Resources["DescriptionStyle"] = labelStyle;
+
+            GradientStop top = new GradientStop(Color.White, .1f);
+            GradientStop bottom = new GradientStop(Color.LightBlue, 1f);
+
+            GradientStopCollection gradientStops = new GradientStopCollection();
+            gradientStops.Add(top);
+            gradientStops.Add(bottom);
+
+            LinearGradientBrush brush = new LinearGradientBrush(gradientStops, new Point(0,0), new Point(0,1));
+
+            Style frameStyle = new Style(typeof(Frame))
+            {
+                Setters =
+                {
+                    new Setter
+                    {
+                        Property=VisualElement.BackgroundProperty,
+                        Value = brush
+                    }
+                }
+            };
+            Resources["FrameStyle"] = frameStyle;
+
+        }
+    }
+}
+
+
