2c98d13dce4b5e5007f0a54966ac01dd45f2571b
John Fecko <john@fecko.me>
2023-06-24 23:32:04 -0400

    Implemented swipe to delete logic

--- Xamurai/Experiments/ListViewPage.xaml.cs
+++ Xamurai/Experiments/ListViewPage.xaml.cs
@@ -12,16 +12,31 @@ namespace Xamurai
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ListViewPage : ContentPage
 	{
+        SampleViewModel viewModel;
 
         public ListViewPage ()
 		{
-			BindingContext = new SampleViewModel();
-			InitializeComponent ();
+            viewModel = new SampleViewModel();
+            BindingContext = viewModel;
+
+            InitializeComponent ();
 		}
 
         void ContentPage_Disappearing(System.Object sender, System.EventArgs e)
         {
             DependencyService.Get<IStatusBar>().ShowStatusBar();
+        }
+
+        async void DeleteItem_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var menuItem = ((MenuItem)sender);
+            Car car = (Car)menuItem.CommandParameter;
+
+            bool answer = await DisplayAlert("Delete " + car.Name + "?", "This will permanently delete " + car.Name, "Delete", "Cancel");
+            if (answer)
+            {
+                viewModel.Cars.Remove(car);
+            }
         }
     }
 }
